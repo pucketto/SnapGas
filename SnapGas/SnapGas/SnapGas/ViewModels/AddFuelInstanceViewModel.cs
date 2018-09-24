a8c4bd3966e7c8726a686b9155f6da2f19e65ddd
reesehp <43527932+reesehp@users.noreply.github.com>
2018-09-23 22:19:37 -0400

    Adding View-Models for Fuel Instances

---
+++ SnapGas/SnapGas/SnapGas/ViewModels/AddFuelInstanceViewModel.cs
@@ -0,0 +1,41 @@
+﻿using SnapGas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapGas.ViewModels
+{
+    public class AddFuelInstanceViewModel : BaseViewModel
+    {
+        public FuelingInstance FuelingInstance { get; set; }
+        public Car SelectedCar { get; set; }
+
+        //private bool addEnabled = false;
+        //public bool AddEnbled
+        //{
+        //    get { return addEnabled; }
+        //    set { SetProperty(ref addEnabled, value); }
+        //}
+
+        public AddFuelInstanceViewModel(FuelingInstance fuelingInstance)
+        {
+            this.SelectedCar = this.SnapGasDb.Cars.Where(c => c.IsSelected == true).FirstOrDefault();
+
+            this.FuelingInstance = fuelingInstance;
+        }
+
+        public async void SaveFuelingInstance()
+        {
+            if (this.FuelingInstance.Id == null || this.FuelingInstance.Id == string.Empty)
+            {
+                this.FuelingInstance.Id = Guid.NewGuid().ToString();
+            }
+            this.FuelingInstance.CarId = this.SelectedCar.Id;
+            this.FuelingInstance.DateFilled = DateTime.Now;
+            this.FuelingInstance.Distance = this.FuelingInstance.NewOdometerReading - this.SelectedCar.OdometerReading;
+            this.SnapGasDb.Save<FuelingInstance>(this.FuelingInstance);
+            this.SelectedCar.OdometerReading = this.FuelingInstance.NewOdometerReading;
+            this.SnapGasDb.Save<Car>(this.SelectedCar);
+        }
+    }
+}
---
+++ SnapGas/SnapGas/SnapGas/ViewModels/FuelingInstancesViewModel.cs
@@ -0,0 +1,86 @@
+﻿using SnapGas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapGas.ViewModels
+{
+    public class FuelingInstancesViewModel : BaseViewModel
+    {
+        private List<FuelingInstance> myFuelingInstances = new List<FuelingInstance>();
+        public List<FuelingInstance> MyFuelingInstances
+        {
+            get { return myFuelingInstances; }
+            set
+            {
+                SetProperty(ref myFuelingInstances, value);
+            }
+        }
+
+        private bool addEnabled = false;
+        public bool AddEnbled
+        {
+            get { return addEnabled; }
+            set { SetProperty(ref addEnabled, value); }
+        }
+
+        private bool hasFuelingInstances = false;
+        public bool HasFuelingInstances
+        {
+            get { return hasFuelingInstances; }
+            set
+            {
+                SetProperty(ref hasFuelingInstances, value);
+            }
+        }
+
+        private bool noFuelingInstances = true;
+        public bool NoFuelingInstances
+        {
+            get { return noFuelingInstances; }
+            set
+            {
+                SetProperty(ref noFuelingInstances, value);
+            }
+        }
+
+        public Car SelectedCar { get; set; }
+
+        public FuelingInstancesViewModel()
+        {
+            if(this.SnapGasDb.Cars.ToList().Count > 0)
+            {
+                var currentCar = this.SnapGasDb.Cars.Where(c => c.IsSelected == true).ToList();
+                this.SelectedCar = currentCar[0];
+                if (currentCar.Count == 1)
+                {
+                    this.AddEnbled = true;
+                }
+            }
+           
+        }
+
+        public void LoadFuelingInstances()
+        {
+            if(this.SnapGasDb.Cars.ToList().Count > 0)
+            {
+                this.MyFuelingInstances = this.SnapGasDb.FuelingInstances.Where(inst => inst.CarId == this.SelectedCar.Id).ToList();
+            }
+            else
+            {
+                this.MyFuelingInstances = this.SnapGasDb.FuelingInstances.ToList();
+            }
+
+            if (this.MyFuelingInstances.Count > 0)
+            {
+                this.HasFuelingInstances = true;
+                this.NoFuelingInstances = false;
+            }
+            else
+            {
+                this.HasFuelingInstances = false;
+                this.NoFuelingInstances = true;
+            }
+        }
+    }
+}
