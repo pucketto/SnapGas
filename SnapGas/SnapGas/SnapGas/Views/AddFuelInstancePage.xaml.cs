407668da4c1fa05fb9421fe624eb75067cd078f5
reesehp <43527932+reesehp@users.noreply.github.com>
2018-09-23 22:21:38 -0400

    Adding the Views for the Fuel Instances Page

---
+++ SnapGas/SnapGas/SnapGas/Views/AddFuelInstancePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SnapGas.Models;
+using SnapGas.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SnapGas.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AddFuelInstancePage : ContentPage
+	{
+        AddFuelInstanceViewModel viewModel;
+
+		public AddFuelInstancePage (FuelingInstance fuelingInstance)
+		{
+            this.viewModel = new AddFuelInstanceViewModel(fuelingInstance);
+
+            if(this.viewModel.SelectedCar != null)
+            {
+                this.Title = this.viewModel.SelectedCar.Make + " " + this.viewModel.SelectedCar.Model;
+            }
+
+            InitializeComponent ();
+            BindingContext = this.viewModel;
+		}
+
+        async void On_SaveTapped(object sender, EventArgs e)
+        {
+            this.viewModel.SaveFuelingInstance();
+            await this.Navigation.PopAsync();
+        }
+    }
+}
---
+++ SnapGas/SnapGas/SnapGas/Views/FuelInstancesPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using SnapGas.Models;
+using SnapGas.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SnapGas.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FuelInstancesPage : ContentPage
+	{
+        FuelingInstancesViewModel viewModel;
+
+		public FuelInstancesPage ()
+		{
+            viewModel = new FuelingInstancesViewModel();
+            this.viewModel.LoadFuelingInstances();
+			InitializeComponent ();
+            BindingContext = this.viewModel;
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.LoadFuelingInstances();
+        }
+
+        async void On_FuelInstanceSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var fuelInstance = args.SelectedItem as FuelingInstance;
+            fuelInstance.IsSelected = true;
+            fuelInstance.NotSelected = false;
+            this.viewModel.SnapGasDb.Save<FuelingInstance>(fuelInstance);
+            foreach (FuelingInstance f in viewModel.MyFuelingInstances.Where(f => f.Id != fuelInstance.Id))
+            {
+                f.NotSelected = true;
+                f.IsSelected = false;
+                this.viewModel.SnapGasDb.Save<FuelingInstance>(f);
+            }
+            this.OnAppearing();
+        }
+
+        async void On_RemoveClicked(object sender, EventArgs e)
+        {
+            //var menuItem = (MenuItem)sender;
+            //var reminder = (Car)menuItem.CommandParameter;
+            //this.viewModel.SnapGasDb.
+            //return;
+        }
+
+        async void On_AddClicked(object sender, EventArgs e)
+        {
+            if(this.viewModel.AddEnbled)
+            {
+                await this.Navigation.PushAsync(new AddFuelInstancePage(new FuelingInstance()));
+            }
+        }
+    }
+}
